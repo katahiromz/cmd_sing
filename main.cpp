b7541728af21574d220c252b6f3f57b95af8fdf6
Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>
2025-01-08 19:26:43 +0900

    update HISTORY.txt

--- main.cpp
+++ main.cpp
@@ -27,7 +27,7 @@ LPCTSTR get_text(INT id)
     {
         switch (id)
         {
-        case 0: return TEXT("cmd_sing バージョン 0.9 by 片山博文MZ");
+        case 0: return TEXT("cmd_sing バージョン 1.0 by 片山博文MZ");
         case 1:
             return TEXT("使い方: cmd_sing [オプション] 文字列\n")
                    TEXT("\n")
@@ -52,7 +52,7 @@ LPCTSTR get_text(INT id)
     {
         switch (id)
         {
-        case 0: return TEXT("cmd_sing version 0.9 by katahiromz");
+        case 0: return TEXT("cmd_sing version 1.0 by katahiromz");
         case 1:
             return TEXT("Usage: cmd_sing [Options] string\n")
                    TEXT("\n")
